1fd100f4b8d78877ebd61feee283699719c1d9a9
AndreiMoraru123 <81184255+AndreiMoraru123@users.noreply.github.com>
2023-12-02 15:56:37 +0200

    black

--- 2023/d1/solution.py
+++ 2023/d1/solution.py
@@ -3,45 +3,46 @@ from typing import Dict
 total_sum: int = 0
 
 digits: Dict[str, str] = {
-    'zero': '0',
-    'one': '1',
-    'two': '2',
-    'three': '3',
-    'four': '4',
-    'five': '5',
-    'six': '6',
-    'seven': '7',
-    'eight': '8',
-    'nine': '9',
+    "zero": "0",
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
 }
+
 
 def get_number(word: str, affix: str) -> str:
     for i in range(len(word)):
         if affix == "suffix":
             root = word[i:]
         elif affix == "prefix":
-            root = word[:i+1]
+            root = word[: i + 1]
         else:
             raise ValueError("not supposed to be here")
         if root in digits:
             return digits[root]
-    return ''
+    return ""
 
 
 def walk(line: str, order: str) -> str:
-    line_sum = ''
-    word = ''
+    line_sum = ""
+    word = ""
     for char in line:
         if char.isnumeric():
             line_sum += char
             break
         else:
-            if order == 'ordinary':
+            if order == "ordinary":
                 word += char
-                num = get_number(word, 'suffix')
-            elif order == 'reversed':
+                num = get_number(word, "suffix")
+            elif order == "reversed":
                 word = char + word
-                num = get_number(word, 'prefix')
+                num = get_number(word, "prefix")
             else:
                 raise ValueError("not supposed to be here")
             if num:
@@ -50,11 +51,11 @@ def walk(line: str, order: str) -> str:
     return line_sum
 
 
-with open("input.txt", 'r') as f:
+with open("input.txt", "r") as f:
     for line in f:
         line = line.strip()
-        ordinary_line_sum = walk(line, 'ordinary')
-        reversed_line_sum = walk(line, 'reversed')
+        ordinary_line_sum = walk(line, "ordinary")
+        reversed_line_sum = walk(line, "reversed")
         line_sum = ordinary_line_sum + reversed_line_sum
         total_sum += int(line_sum)
 
--- 2023/d2/solution.py
+++ 2023/d2/solution.py
@@ -4,13 +4,15 @@ from typing import Dict, Tuple
 from functools import reduce
 
 rules: Dict[str, int] = {
-    'blue': 14,
-    'green': 13,
-    'red': 12,
+    "blue": 14,
+    "green": 13,
+    "red": 12,
 }
 
+
 def build_number(sequence: str) -> str:
-    return ''.join([char for char in sequence if char.isnumeric()])
+    return "".join([char for char in sequence if char.isnumeric()])
+
 
 def validate(color: str, draw: str) -> bool:
     if color in draw:
@@ -18,21 +20,28 @@ def validate(color: str, draw: str) -> bool:
         return int(complete_number) <= rules[color]
     return True
 
+
 def process_game_line(line: str) -> Tuple[str, bool, int]:
-    game_id, cubes_info = line.strip().split(':')
+    game_id, cubes_info = line.strip().split(":")
     nr = build_number(game_id)
     min_cubes = {color: 0 for color in rules}
-    sets = cubes_info.split(';')
+    sets = cubes_info.split(";")
 
-    draws = list(itertools.chain.from_iterable(map(lambda s: s.split(','), sets)))
-    min_cubes = {color: max((int(build_number(draw)) for draw in draws if color in draw), default=0) for color in rules}
+    draws = list(itertools.chain.from_iterable(map(lambda s: s.split(","), sets)))
+    min_cubes = {
+        color: max(
+            (int(build_number(draw)) for draw in draws if color in draw), default=0
+        )
+        for color in rules
+    }
     power = reduce(operator.mul, min_cubes.values())
     is_valid_game = all(validate(color, draw) for draw in draws for color in rules)
     return nr, is_valid_game, power
 
+
 OK_GAME: Dict[str, bool] = {}
 
-with open('input.txt', 'r') as f:
+with open("input.txt", "r") as f:
     min_viable_game_line = 0
     for line in f:
         game_number, is_valid, power = process_game_line(line)
@@ -41,4 +50,3 @@ with open('input.txt', 'r') as f:
 
 print(sum(int(key) for key, value in OK_GAME.items() if value))
 print(min_viable_game_line)
-        
