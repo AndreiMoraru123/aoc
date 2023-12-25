1662ecb3513de63bdbeaeedea8afa668b3905674
AndreiMoraru123 <81184255+AndreiMoraru123@users.noreply.github.com>
2023-12-25 03:28:14 +0200

    day 19 rework

--- 2023/d19/solution.py
+++ 2023/d19/solution.py
@@ -1,27 +1,28 @@
 from collections import deque
 
-workflows, parts = open('input.txt').read().split('\n\n')
-criteria = {'A': 'A', 'R': 'R'}
+workflows, parts = open("input.txt").read().split("\n\n")
+criteria = {"A": "A", "R": "R"}
 rules = {}
 for line in workflows.splitlines():
-    name, ratings = line[:-1].split('{')
-    ratings = ratings.replace(':', ' and ').replace(',', ' or ')
+    name, ratings = line[:-1].split("{")
+    ratings = ratings.replace(":", " and ").replace(",", " or ")
     rules[name] = ratings
     criteria[name] = name
 
 ans = 0
 for p in parts.splitlines():
-    x, m, a, s = [int(el[2:]) for el in p[1:-1].split(',')]
-    curr = 'in'
-    while curr not in 'AR':
-        curr = eval(rules[curr], criteria,  locals())
-    if curr == 'A':
+    x, m, a, s = [int(el[2:]) for el in p[1:-1].split(",")]
+    curr = "in"
+    while curr not in "AR":
+        curr = eval(rules[curr], criteria, locals())
+    if curr == "A":
         ans += x + m + a + s
 
 print(ans)
 
+
 class Range:
-    def __init__(self, name, range = None) -> None:
+    def __init__(self, name, range=None) -> None:
         self.name = name
         self.range = range
 
@@ -31,6 +32,9 @@ class Range:
     def __gt__(self, other):
         return Range(self.name, range(other + 1, 4001))
 
+    def __repr__(self):
+        return f"name={self.name}, range={self.range}"
+
 
 def split_range(a, b):
     sect = range(max(a.start, b.start), min(a.stop, b.stop))
@@ -39,37 +43,41 @@ def split_range(a, b):
     return left, sect, right
 
 
-criteria = {'A': 'A', 'R': 'R'}
+criteria = {"A": "A", "R": "R"}
 for line in workflows.splitlines():
-    name, _ = line[:-1].split('{')
+    name, _ = line[:-1].split("{")
     criteria[name] = name
 
 rules = {}
 for line in workflows.splitlines():
-    name, ratings = line.split('{')
-    x, m, a, s = Range('x'), Range('m'), Range('a'), Range('s')
-    ratings = '{' + ',None:'.join(ratings.rsplit(',', 1))
+    name, ratings = line.split("{")
+    x, m, a, s = Range("x"), Range("m"), Range("a"), Range("s")
+    ratings = "{" + ",None:".join(ratings.rsplit(",", 1))
     rules[name] = eval(ratings, globals(), criteria)
 
 q = deque()
-q.append(('in', *[range(1, 4001) for _ in range(4)]))
+q.append(("in", *[range(1, 4001) for _ in range(4)]))
 ans = 0
 
 while q:
     curr, x, m, a, s = q.popleft()
-    if curr == 'R':
+    if curr == "R":
         continue
-    if curr == 'A':
+    if curr == "A":
         ans += len(x) * len(a) * len(m) * len(s)
         continue
     for k, v in rules[curr].items():
         if k is None:
             q.append((v, x, m, a, s))
             continue
-        left, sect, right, = split_range(eval(k.name), k.range)
-        assert not(left and right)
-        exec(f'global {k.name}; {k.name} = sect')
+        (
+            left,
+            sect,
+            right,
+        ) = split_range(eval(k.name), k.range)
+        assert not (left and right)
+        exec(f"global {k.name}; {k.name} = sect")
         q.append((v, x, m, a, s))
-        exec(f'global {k.name}; {k.name} = left or right')
+        exec(f"global {k.name}; {k.name} = left or right")
 
 print(ans)
